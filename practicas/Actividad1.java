39158d1342c780e6dcf1698de6b75423aa5abd8b
benitocam <re_gistroscuentras@hotmail.com>
2014-09-26 20:18:26 -0500

    Update Actividad1.java

--- practicas/Actividad1.java
+++ practicas/Actividad1.java
@@ -12,4 +12,6 @@ public class Actividad1 {
   
     public static void main (String args []){
   System.out.println("Hola este es el primer archivo subido a Github");
-}}
+  System.out.println("Prueba de descarga");
+      
+    }}
